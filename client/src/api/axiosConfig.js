6cba5094c1edc8f36c0448a7d7e0ddb7f8d236d9
Himani Jangid <himanijangid07@gmail.com>
2025-05-26 15:34:53 +0530

    some changes

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API || 'http://localhost:8080'
+    baseURL: process.env.REACT_APP_API || 'http://localhost:8080',
+    withCredentials: true,
 });
 
 axiosInstance.interceptors.request.use(
