e6bb82c4b4dfa5ff3eaafbceab3ef52085ebb8f0
Himani Jangid <himanijangid07@gmail.com>
2025-05-26 14:26:27 +0530

    client completed

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080', // your backend server address
+    baseURL: process.env.REACT_APP_API || 'http://localhost:8080', // your backend server address
     withCredentials: true, // if you are using cookies or sessions
 });
 
