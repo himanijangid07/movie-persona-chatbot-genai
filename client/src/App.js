03984cdf917fa03abb43d5f8169df7546397fc62
Himani Jangid <himanijangid07@gmail.com>
2025-05-27 12:47:32 +0530

    some change

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,18 @@
 import './App.css';
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Homepage from './pages/Homepage';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import { useMemo } from 'react';
 import { themeSettings } from "./theme";
-import {CssBaseline, ThemeProvider} from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
-import {Toaster} from 'react-hot-toast'
-import ChatPage from './pages/ChatPage'
-
+import { Toaster } from 'react-hot-toast';
+import ChatPage from './pages/ChatPage';
 
 function App() {
-  const theme = useMemo(() => createTheme(themeSettings()), [])
+  const theme = useMemo(() => createTheme(themeSettings()), []);
 
   return (
     <ThemeProvider theme={theme}>
@@ -23,7 +22,8 @@ function App() {
         <Navbar />
         <div style={{ flex: 1, overflow: 'hidden' }}>
           <Routes>
-            <Route path="/" element={<Homepage />} />
+            <Route path="/" element={<Navigate to="/register" />} />
+            <Route path="/homepage" element={<Homepage />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
             <Route path="/chat" element={<ChatPage />} />
